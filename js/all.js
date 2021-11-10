981a07fb8e6193d8890a0f0d4b3e7315e5e14e54
kyriehavegoodday <healthymbear@gmail.com>
2021-11-10 17:17:52 +0800

    首頁連結其他頁面完成

--- js/all.js
+++ js/all.js
@@ -9,3 +9,22 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
+let select = document.getElementById('search_select');
+let search_a = document.getElementById('search_a');
+search_a.addEventListener('click',(e)=>{
+  e.preventDefault();
+  
+  let place = search_select.place.value;
+  if(place ==='taipei'){
+    window.location.href='taipei/';
+  }
+  if(place ==='newTaipei'){
+    window.location.href='NewTaipei/';
+  }
+  if(place ==='tainan'){
+    window.location.href='Tainan/';
+  }
+  if(place ==='kaohsiung'){
+    window.location.href='Kaohsiung/';
+  }
+})
