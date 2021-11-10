8baae9767784d3a3cc2c408934735bad3ddb66df
kyriehavegoodday <healthymbear@gmail.com>
2021-11-10 18:45:45 +0800

    台北景點頁面api資料串接完成

--- js/all.js
+++ js/all.js
@@ -9,22 +9,52 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
-let select = document.getElementById('search_select');
-let search_a = document.getElementById('search_a');
-search_a.addEventListener('click',(e)=>{
-  e.preventDefault();
+  let select = document.getElementById('search_select');
+  let search_a = document.getElementById('search_a');
+  search_a.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      let place = search_select.place.value;
+      if (place === 'taipei') {
+          window.location.href = '../taipei/';
+      }
+      if (place === 'newTaipei') {
+          window.location.href = '../NewTaipei/';
+      }
+      if (place === 'tainan') {
+          window.location.href = '../Tainan/';
+      }
+      if (place === 'kaohsiung') {
+          window.location.href = '../Kaohsiung/';
+      }
+  })
+
+  // 台北旅遊api資料
+  var dataUrl= "https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/Taipei?$top=30&$format=JSON"
+  var xhr = new XMLHttpRequest()
+  xhr.open('GET',dataUrl, true)
+  xhr.send()
+  xhr.onload = function(){
+      var data = JSON.parse(this.responseText);
+      console.log(data);
+      var nav_home = document.getElementById('nav_home');
+      var str = '';
+      
+      for(item in data){
+        
+        str=
+        `
+        <a href="${data[item].WebsiteUrl}">
+                <div class="view_card">
+                    <img src="${data[item].Picture.PictureUrl1}">
+                    <div class="view_text">
+                        <div>${data[item].Name}</div>
+                        <p>${data[item].DescriptionDetail}</p>
+                    </div>
+                </div>
+            </a>
+        `
+        nav_home.innerHTML +=str;
+      }
+  }
   
-  let place = search_select.place.value;
-  if(place ==='taipei'){
-    window.location.href='taipei/';
-  }
-  if(place ==='newTaipei'){
-    window.location.href='NewTaipei/';
-  }
-  if(place ==='tainan'){
-    window.location.href='Tainan/';
-  }
-  if(place ==='kaohsiung'){
-    window.location.href='Kaohsiung/';
-  }
-})
