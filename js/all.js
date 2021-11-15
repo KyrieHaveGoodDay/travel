3d700fa5422db641b9becfa2fc80ab22d634a810
kyriehavegoodday <healthymbear@gmail.com>
2021-11-15 16:27:44 +0800

    加入預載效果及調整版頭動畫細節

--- js/all.js
+++ js/all.js
@@ -1,3 +1,21 @@
+$(document).ready(function () {
+    setTimeout(function () {
+        $('.load-wrapper').fadeOut(500);
+    }, 1000)
+
+    setTimeout(function () {
+        // 桌機版箭頭跟名字
+        gsap.to('.swiper-button', { duration: 0.3, scale: 3.5, repeat: 3, yoyo: true });
+        gsap.to('.title_name', { duration: 0.5, y: -20, repeat: -1, yoyo: true });
+
+        // 手機選單
+        const t1 = gsap.timeline({});
+        t1.to('.nav_list', { duration: 0.5, height: 84, })
+            .to('.nav_list>a', { duration: 0.5, keyframes: [{ scale: 1.5 }, { scale: 1 }], stagger: 0.1 }, 0.3);
+    }, 1200)
+
+
+})
 
 var swiper = new Swiper(".mySwiper", {
 
@@ -23,11 +41,4 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
-
-
-// 放大縮小 再換下一個
-
-const t1 = gsap.timeline({});
-t1.to('.nav_list',{duration:0.5,height:84,})
-.to('.nav_list>a' , {duration:0.5,keyframes:[{scale:1.5},{scale:1}],stagger:0.1},0.3);
 
